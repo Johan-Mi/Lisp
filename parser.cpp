e3797f3427add1479066c6c1039ecd58d152080d
Johan-Mi <johanmilanov@outlook.com>
2020-09-06 11:56:29 +0200

    -

--- parser.cpp
+++ parser.cpp
@@ -23,8 +23,8 @@ std::optional<std::tuple<Symbol, TokenIter>> parse_symbol(
 
 std::optional<std::tuple<Quote, TokenIter>> parse_quoted_expression(
 		TokenIter begin, TokenIter end) {
-	if(auto const a = parse_quote(begin, end); a) {
-		if(auto const b = parse_expression(*a, end); b) {
+	if(auto const a = parse_quote(begin, end)) {
+		if(auto const b = parse_expression(*a, end)) {
 			return {{Quote{std::get<std::shared_ptr<Object const>>(*b)},
 					std::get<TokenIter>(*b)}};
 		}
@@ -35,7 +35,7 @@ std::optional<std::tuple<Quote, TokenIter>> parse_quoted_expression(
 
 std::optional<std::tuple<Cons, TokenIter>> parse_cons(
 		TokenIter begin, TokenIter end) {
-	if(auto const a = parse_lparen(begin, end); a) {
+	if(auto const a = parse_lparen(begin, end)) {
 		return parse_cons_helper(*a, end);
 	} else {
 		return std::nullopt;
@@ -44,13 +44,12 @@ std::optional<std::tuple<Cons, TokenIter>> parse_cons(
 
 std::optional<std::tuple<Cons, TokenIter>> parse_cons_helper(
 		TokenIter begin, TokenIter end) {
-	if(auto const a = parse_rparen(begin, end); a) {
+	if(auto const a = parse_rparen(begin, end)) {
 		return {{make_nil(), *a}};
-	} else if(auto const a = parse_expression(begin, end); a) {
-		if(auto const b = parse_dot(std::get<TokenIter>(*a), end); b) {
-			if(auto const c = parse_expression(*b, end); c) {
-				if(auto const d = parse_rparen(std::get<TokenIter>(*c), end);
-						d) {
+	} else if(auto const a = parse_expression(begin, end)) {
+		if(auto const b = parse_dot(std::get<TokenIter>(*a), end)) {
+			if(auto const c = parse_expression(*b, end)) {
+				if(auto const d = parse_rparen(std::get<TokenIter>(*c), end)) {
 					return {{Cons{std::get<std::shared_ptr<Object const>>(*a),
 									 std::get<std::shared_ptr<Object const>>(
 											 *c)},
@@ -58,8 +57,7 @@ std::optional<std::tuple<Cons, TokenIter>> parse_cons_helper(
 				}
 			}
 		} else if(auto const b
-				  = parse_cons_helper(std::get<TokenIter>(*a), end);
-				  b) {
+				= parse_cons_helper(std::get<TokenIter>(*a), end)) {
 			return {{Cons{std::get<std::shared_ptr<Object const>>(*a),
 							 std::make_shared<Object const>(
 									 std::get<Cons>(*b))},
@@ -72,16 +70,16 @@ std::optional<std::tuple<Cons, TokenIter>> parse_cons_helper(
 
 std::optional<std::tuple<std::shared_ptr<Object const>, TokenIter>>
 parse_expression(TokenIter begin, TokenIter end) {
-	if(auto const a = parse_cons(begin, end); a) {
+	if(auto const a = parse_cons(begin, end)) {
 		return {{std::make_shared<Object const>(std::get<Cons>(*a)),
 				std::get<TokenIter>(*a)}};
-	} else if(auto const a = parse_quoted_expression(begin, end); a) {
+	} else if(auto const a = parse_quoted_expression(begin, end)) {
 		return {{std::make_shared<Object const>(std::get<Quote>(*a)),
 				std::get<TokenIter>(*a)}};
-	} else if(auto const a = parse_integer(begin, end); a) {
+	} else if(auto const a = parse_integer(begin, end)) {
 		return {{std::make_shared<Object const>(std::get<Integer>(*a)),
 				std::get<TokenIter>(*a)}};
-	} else if(auto const a = parse_symbol(begin, end); a) {
+	} else if(auto const a = parse_symbol(begin, end)) {
 		return {{std::make_shared<Object const>(std::get<Symbol>(*a)),
 				std::get<TokenIter>(*a)}};
 	} else {
